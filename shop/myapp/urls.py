0f5c7e0e8680eb065e5cfde402ab0a95697aed1f
faithit <faithittrainer@gmail.com>
2026-05-14 15:41:09 +0300

    info

--- shop/myapp/urls.py
+++ shop/myapp/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path('addproduct/',views.add_product,name='addproduct'),
     path('home/',views.home,name='home'),
     path('callback/',views.callback,name='callback'),
-    path('pay/',views.lipa_na_mpesa_online,name='pay')
+    path('pay/',views.lipa_na_mpesa_online,name='pay'),
+    path('paymentinfo/',views.payment_info,name='paymentinfo'),
 
 ]
 if settings.DEBUG:
--- shop/myapp/views.py
+++ shop/myapp/views.py
@@ -29,8 +29,9 @@ def lipa_na_mpesa_online(request):
        callback_url="https://71d7-41-203-217-161.ngrok-free.app/callback"
        cl=MpesaClient()
        response=cl.stk_push(phone_number,amount,account_reference,transaction_desc,callback_url)
-       return redirect('index')
-       return HttpResponse("paymet successful",response)
+
+       return HttpResponse("payment successful",response)
+
     return HttpResponse("Invalid request")
 
 @csrf_exempt
@@ -40,4 +41,6 @@ def callback(request):
        print("callback data",data)
        return JsonResponse({"status":"success"})
     return JsonResponse({"status":"Invalid request"})
+def payment_info(request):
+    return render(request,'paymentinfo.html')
 
